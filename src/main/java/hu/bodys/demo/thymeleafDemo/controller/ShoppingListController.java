a47788ffbde40f96582b4f17c087ad3a35370853
Bódis Tamás <bodistamas90@gmail.com>
2022-03-08 18:11:45 +0100

    introduce containers and extracted parts of page

--- src/main/java/hu/bodys/demo/thymeleafDemo/controller/ShoppingListController.java
+++ src/main/java/hu/bodys/demo/thymeleafDemo/controller/ShoppingListController.java
@@ -1,6 +1,8 @@
 package hu.bodys.demo.thymeleafDemo.controller;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,15 +16,24 @@ public class ShoppingListController {
 
     @GetMapping("/")
     public String homePage(Model model){
-        model.addAttribute("name", "Tamás");
-
         ShoppingListItem item1 = new ShoppingListItem("Bread", 1);
         ShoppingListItem item2 = new ShoppingListItem("Milk", 3);
         ShoppingListItem item3 = new ShoppingListItem("Candy", 1);
-        
         ShoppingList shoppingList = new ShoppingList("Shopping 1", LocalDate.now());
         shoppingList.addAll(item1, item2, item3);
-        model.addAttribute("shoppingList", shoppingList);
+
+        ShoppingListItem item4 = new ShoppingListItem("Sliced Bread", 1);
+        ShoppingListItem item5 = new ShoppingListItem("Milk", 3);
+        ShoppingListItem item6 = new ShoppingListItem("Sausage", 2);
+        ShoppingListItem item7 = new ShoppingListItem("Water", 6);
+        ShoppingList shoppingList2 = new ShoppingList("Shopping 2", LocalDate.now().minusDays(2));
+        shoppingList2.addAll(item4, item5, item6, item7);
+        
+        
+        List<ShoppingList> shoppingLists = new ArrayList<>();
+        shoppingLists.add(shoppingList);
+        shoppingLists.add(shoppingList2);
+        model.addAttribute("shoppingLists", shoppingLists);
 
         return "index";
 
